2522a2d977cfdca85ec8da8ff02be5e9039b1d2c
rachid <rachidboudaoudi95@gmail.com>
2021-08-19 21:30:30 +0100

    Add style to the List

--- src/main-src.js
+++ src/main-src.js
@@ -21,22 +21,21 @@ const tasks = [
 
 function showList(tasksList) {
   const listDiv = document.getElementById('list');
-  for (let i = 0; i < tasksList.length; i += 1) {
+  for (let index = 0; index < tasksList.length; index += 1) {
     const check = document.createElement('input');
     const task = document.createElement('li');
     const i = document.createElement('i');
     const description = document.createElement('p');
     check.type = 'checkbox';
-    console.log( tasksList[i]);
-    check.checked = tasksList[i].completed;
+    check.checked = tasksList[index].completed;
     i.className = 'fas fa-ellipsis-v';
     description.className = 'description';
     description.appendChild(check);
-    // description.innerHTML += ` ${tasksList[i].description}`;
+    description.innerHTML += ` ${tasksList[index].description}`;
     task.appendChild(description);
     task.appendChild(i);
     listDiv.appendChild(task);
   }
 }
 
-showList([{nam : 'task', description:'fsss',}, {name : 'task', description : 'task description'}]);
+showList(tasks);
