876dd1c36b12039900854e90a5f7cb936e7a8121
rachid <rachidboudaoudi95@gmail.com>
2021-08-21 18:30:11 +0100

    Refactor the main js to use one eventListener for update/remove tasks

--- src/main-src.js
+++ src/main-src.js
@@ -11,38 +11,32 @@ let tasks = getStorage();
 
 showList(tasks);
 
-// update the task status --------------------------------------------------
+// update the task status && remove a task --------------------------------------------------
 
 const listDiv = document.getElementById('list');
-listDiv.addEventListener('change', (event) => {
-  if (event.target !== event.currentTarget) {
-    updateStatus(tasks, parseInt(event.target.parentElement.parentElement.id, 10), true);
-    showList(tasks);
-    updateStorage(tasks);
-  }
-  event.stopPropagation();
-});
-
 listDiv.addEventListener('click', (event) => {
-  if (event.target !== event.currentTarget && event.target.className === 'fas fa-check') {
-    updateStatus(tasks, parseInt(event.target.parentElement.parentElement.id, 10), false);
-    showList(tasks);
-    updateStorage(tasks);
+  if (event.target !== event.currentTarget) {
+    if (event.target.className === 'check') {
+      updateStatus(tasks, parseInt(event.target.parentElement.parentElement.id, 10), true);
+      showList(tasks);
+      updateStorage(tasks);
+    }
+    if (event.target.className === 'fas fa-check') {
+      updateStatus(tasks, parseInt(event.target.parentElement.parentElement.id, 10), false);
+      showList(tasks);
+      updateStorage(tasks);
+    }
+    if (event.target.className === 'fas fa-trash') {
+      tasks = removeTask(tasks, parseInt(event.target.parentElement.id, 10));
+    }
   }
   event.stopPropagation();
 });
 
 // add a task ----------------------------------------------------------------
-const addTaskInput = document.getElementById('add');
-addTaskInput.addEventListener('click', () => {
+const addTaskButton = document.getElementById('add');
+addTaskButton.addEventListener('click', () => {
   addTask(tasks);
   updateStorage(tasks);
   showList(tasks);
-});
-
-// remove a task
-listDiv.addEventListener('click', (event) => {
-  if (event.target !== event.currentTarget && event.target.className === 'fas fa-trash') {
-    tasks = removeTask(tasks, parseInt(event.target.parentElement.id, 10));
-  }
 });
